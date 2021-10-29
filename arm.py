ebb7f071017a39ec41bb6797a8ebf4b9954e50ea
LAPTOP-NK541KUC\emree <emreerim@hotmail.nl>
2021-10-29 23:18:58 +0200

    removed example code for kinematics calculations

--- arm.py
+++ arm.py
@@ -36,73 +36,30 @@ class arm_segment:
         return self.mount_angle + self.angle
 
     def rotate_to(self,target):
-        def distance_to_default(angle, *args): 
-            # weights found with trial and error, get some wrist bend, but not much
-            weight = [1, 1, 1.3] 
-            #print("distance:",np.sqrt(np.sum([(qi - q0i)**2 * wi
-            #    for qi,q0i,wi in zip(angle, self.mount_position, weight)])))
-            return np.sqrt(np.sum([(qi - q0i)**2 * wi
-                for qi,q0i,wi in zip(angle, self.mount_position, weight)]))
-
-        def x_constraint(angle, xy):
-            x = (self.length*np.cos(angle)) - xy[0]
-            #print("x:", x)
-            return x
-
-        def y_constraint(angle, xy): 
-            y = (self.length*np.sin(angle)) - xy[1]
-            #print("y:", y)
-            return y
-        
-        #print("optimize",scipy.optimize.fmin_slsqp(
-        #    func=distance_to_default,
-        #    x0=self.angle,
-        #    eqcons=[x_constraint,
-        #            y_constraint],
-            # uncomment to add in min / max angles for the joints
-            # ieqcons=[joint_limits_upper_constraint,
-            #          joint_limits_lower_constraint],
-        #    args=(target,),
-        #    iprint=0))  # iprint=0 suppresses output                print("Arm length {0} angle {1} mount {2} endpoint {3} ".format(self.length,self.angle,self.mount_position, self.end_position))
-        #print("Arm length {0} angle {1} mount {2} endpoint {3} ".format(self.length,self.angle,self.mount_position, self.end_position))
-
-        
-        return scipy.optimize.fmin_slsqp(
-            func=distance_to_default,
-            x0=self.angle,
-            eqcons=[x_constraint,
-                    y_constraint],
-            # uncomment to add in min / max angles for the joints
-            # ieqcons=[joint_limits_upper_constraint,
-            #          joint_limits_lower_constraint],
-            args=(target,),
-            iprint=0)  # iprint=0 suppresses output
-
-        #pass
         # TODO:
         # step 1: move vectors to origin
-        #target_vector = np.array([0.0,0.0,0.0])
-        #target_vector[0] = target[0] - self.mount_position[0]
-        #target_vector[1] = target[1] - self.mount_position[1]
-        #target_vector[2] = target[2] - self.mount_position[2]
+        target_vector = np.array([0.0,0.0,0.0])
+        target_vector[0] = target[0] - self.mount_position[0]
+        target_vector[1] = target[1] - self.mount_position[1]
+        target_vector[2] = target[2] - self.mount_position[2]
 
-        #current_vector = np.array([0.0,0.0,0.0])
-        #current_vector[0] = current[0] - self.mount_position[0]
-        #current_vector[1] = current[1] - self.mount_position[1]
-        #current_vector[2] = current[2] - self.mount_position[2]
+        current_vector = np.array([0.0,0.0,0.0])
+        current_vector[0] = self.end_position[0] - self.mount_position[0]
+        current_vector[1] = self.end_position[1] - self.mount_position[1]
+        current_vector[2] = self.end_position[2] - self.mount_position[2]
 
         # step 2: calculate nomalized dot
-        #normalized_target = target_vector/np.linalg.norm(target_vector)
-        #normalized_current = current_vector/np.linalg.norm(current_vector)
-        #dot = np.dot(normalized_target,normalized_current)
+        normalized_target = target_vector / np.sqrt(np.sum(target_vector**2))
+        normalized_current = current_vector / np.sqrt(np.sum(current_vector**2))
+        dot = np.dot(normalized_current, normalized_target)
 
         # step 3: calc cross for direction
-        #cross = np.cross(normalized_target,normalized_current)
+        cross = np.cross(normalized_current, normalized_target)
+
+        print("dot: {0}, cross {1}".format(dot, cross))
 
         # update angle based on dot watch constraints
         #self.angle = dot
-
-
 
 class robot_arm:
     def __init__(self) -> None:
@@ -126,19 +83,15 @@ class robot_arm:
             self.end_angle = s.get_combined_angle()
             self.Xcoords = np.append(self.Xcoords, self.end_position[0])
             self.Ycoords = np.append(self.Ycoords, self.end_position[1])
-    
+
     def move_to(self,pos):
-        maximum_loopcount = 20 
+        maximum_loopcount = 20
+        #print("pos {0}, end_position {1}". format(pos, self.end_position))
         while  np.array_equal(pos,self.end_position) == False:
-            #print(1)
             for seg in reversed(self.segments):
-                #print(2)
                 seg.rotate_to(pos)
-           # print(3)
             maximum_loopcount -= 1
-            #print(4)
-            if maximum_loopcount == 0: break  
-            #print(5)
+            if maximum_loopcount == 0: break
 
     def print(self):
         self.calc_endpoint()
@@ -155,14 +108,14 @@ arm.add_segment(arm_segment(80))
 
 arm.print()
 #print(arm.end_position[0], arm.end_position[1])
-print(arm.move_to([200.0, 100.0]))
+#print(arm.move_to([200.0, 100.0]))
 
 #arm.print()
 """A function for plotting an arm, and having it calculate the 
 inverse kinematics such that given the mouse (x, y) position it 
 finds the appropriate joint angles to reach that point."""
 
-# make our window for drawin'
+# make our window for drawing
 window = pyglet.window.Window()
 
 label = pyglet.text.Label('Mouse (x,y)', font_name='Times New Roman', 
@@ -218,6 +171,6 @@ def on_mouse_motion(x, y, dx, dy):
     xy = np.array([x - window.width/2, y, 0.0])
     #print(xy)
 
-    #arm.move_to(xy) # get new joint (x,y) positions
+    arm.move_to(xy) # get new joint (x,y) positions
 
 pyglet.app.run()
